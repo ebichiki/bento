a350cebe62726258ba7cc1afa66c9a9286f90742
ebichiki <allegro.con.spirito28@gmail.com>
2019-01-30 21:41:21 +0900

    create model item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_145546) do
+ActiveRecord::Schema.define(version: 2019_01_30_122450) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item.create!(
+  [
+   {
+    name: 'お子様御膳',
+    price: 500,
+   },
+   {
+    name: 'お子様弁当',
+    price: 400,
+   },
+   {
+    name: 'キッズプレート',
+    price: 300,
+   }
+  ]
+)

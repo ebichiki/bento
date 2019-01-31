c89d53ed360c3da6c484c91bd75fcae623a0e774
ebichiki <allegro.con.spirito28@gmail.com>
2019-01-31 23:01:24 +0900

    associate user item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_30_131315) do
+ActiveRecord::Schema.define(version: 2019_01_31_135219) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2019_01_30_131315) do
     t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "item_id"
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|

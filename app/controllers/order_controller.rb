fd486d0fde796caf3b34fb59348fe96986c41525
ebichiki <allegro.con.spirito28@gmail.com>
2019-02-11 10:41:34 +0900

    update order

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,40 +1,24 @@
 class OrderController < ApplicationController
-  respond_to :html
+
   def index
     now = Time.current
-    @target_days = (now.beginning_of_month.to_date)..(now.end_of_month.to_date)
-    @items = Item.order('id')
-    orders = Order.where(user_id: current_user.id)
+    @row_headers = (now.beginning_of_month.to_date)..(now.end_of_month.to_date)
+    @column_headers = Item.select(:name, :price).order(:id)
+    @orders = Order.select(:id, :item_id, :date, :number)
+                  .where('user_id = ? and date >= ? and date <= ? ',
+                         current_user.id, now.at_beginning_of_month.to_date, now.end_of_month.to_date)
+                  .order(:date, :item_id)
+                  .group_by {|o| o[:date]}
 
-    # @monthly_lists = Array.new(@target_days.count, Array.new(@items.count, 0))
-    @monthly_lists = Array.new(@target_days.count).map { Array.new(@items.count, 0)}
-    print(@monthly_lists)
-    id_list = []
-    @items.each do |item|
-      id_list.push(item.id)
-    end
-
-    orders.each do |order|
-      row = (order.date - now.beginning_of_month.to_date).to_i
-      col = id_list.index(order.item_id)
-      @monthly_lists[row][col] = order.number
-
-    end
   end
 
   def create
     print('a')
   end
-  
+
   def update
-    # @order = Order.where(user_id: params[:id])
-    # now = Time.current
-    #  (order.date - now.beginning_of_month.to_date).to_i do |days|
-    #   id_list.index(order.item_id) do |item|
-    #       order = Order.find_by(id: days, item_id: item)
-    #       order.update_attributes(number: @monthly_lists[days][item])
-    #       redirect_to request.referer
-      # end
-    # end
+    order = Order.find(params[:id])
+    order.update(number: params[:order][:number])
+    redirect_to root_path, notice: "注文情報を更新しました。 注文日: #{order.date}"
   end
 end

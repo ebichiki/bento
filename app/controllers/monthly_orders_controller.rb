b43bdb64db1ba4cafeb69b5cfd8f4f658b35f3e2
ebichiki <allegro.con.spirito28@gmail.com>
2019-02-11 23:41:05 +0900

    show monthly_orders

--- app/controllers/monthly_orders_controller.rb
+++ app/controllers/monthly_orders_controller.rb
@@ -1,7 +1,12 @@
 class MonthlyOrdersController < ApplicationController
 
   def index
-    now = Time.current
+    now = Date.today.strftime('%Y%m')
+    redirect_to action: :show, yyyymm: now
+  end
+  
+  def show
+    now = Date.strptime(params[:yyyymm]+"01",'%Y%m%d')
     @row_headers = (now.beginning_of_month.to_date)..(now.end_of_month.to_date)
     @column_headers = Item.select(:name, :price).order(:id)
     @orders = Order.select(:id, :item_id, :date, :number)
@@ -9,7 +14,6 @@ class MonthlyOrdersController < ApplicationController
                          current_user.id, now.at_beginning_of_month.to_date, now.end_of_month.to_date)
                   .order(:date, :item_id)
                   .group_by {|o| o[:date]}
-
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :monthly_orders
+  resources :monthly_orders, param: :yyyymm
   resources :order
   root 'monthly_orders#index'
   # put :order, to: 'order#update_all'

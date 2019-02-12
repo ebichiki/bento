fb27e4b4b1e9cd9d1ea70ff5b849dfa3b2b90822
ebichiki <allegro.con.spirito28@gmail.com>
2019-02-12 23:10:34 +0900

    create monthlyorders

--- app/controllers/monthly_orders_controller.rb
+++ app/controllers/monthly_orders_controller.rb
@@ -9,6 +9,11 @@ class MonthlyOrdersController < ApplicationController
     now = Date.strptime(params[:yyyymm]+"01",'%Y%m%d')
     @row_headers = (now.beginning_of_month.to_date)..(now.end_of_month.to_date)
     @column_headers = Item.select(:name, :price).order(:id)
+    monthlyorders = Order.where('user_id = ? and date >= ? and date <= ? ',
+                         current_user.id, now.at_beginning_of_month.to_date, now.end_of_month.to_date).count
+    if monthlyorders == 0
+      create_monthlyorders(params[:yyyymm])
+    end
     @orders = Order.select(:id, :item_id, :date, :number)
                   .where('user_id = ? and date >= ? and date <= ? ',
                          current_user.id, now.at_beginning_of_month.to_date, now.end_of_month.to_date)
@@ -16,4 +21,23 @@ class MonthlyOrdersController < ApplicationController
                   .group_by {|o| o[:date]}
   end
 
+  def create_monthlyorders(yyyymm)
+    month = Date.strptime(yyyymm+"01",'%Y%m%d')
+    (month.beginning_of_month.to_date..month.end_of_month.to_date).each do |days|
+      Order.transaction do
+        Item.all.each do |item|
+          Order.create!(
+            [
+              {
+                date: days,
+                number: 0,
+                item_id: item.id,
+                user_id: current_user.id
+              }
+            ]
+          )
+        end
+      end
+    end   
+  end
 end

acc2c214e447ca2c9a4512f8d14bf77118223420
ebichiki <allegro.con.spirito28@gmail.com>
2019-02-23 08:51:13 +0900

    rspec test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # 商品名、金額があれば有効
+  it 'is valid with name, price' do
+    item = Item.new(
+      name: 'のり弁当',
+      price: 100
+    )
+    expect(item).to be_valid
+  end
 end

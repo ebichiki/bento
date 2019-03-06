f9e06ec075a80d3ff0d366505b3f00d8738bd3ef
ebichiki <allegro.con.spirito28@gmail.com>
2019-03-06 22:35:31 +0900

    モジュール名変更

--- spec/helpers/monthly_orders_helper_spec.rb
+++ spec/helpers/monthly_orders_helper_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe MonthlyOrdersHelper, type: :helper do
-  describe "editMonth" do
-    it { expect(editMonth('201902')).to eq('2019-02') }
+  describe 'edit_month' do
+    it { expect(edit_month('201902')).to eq('2019-02') }
   end
 end
